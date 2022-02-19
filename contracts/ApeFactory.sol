f3dc8367c58ac3623b1bca3d348e5f23e2ce3512
ckt22 <tangchekkit@kidpairing.com>
2022-02-19 16:43:36 +0800

    Contract deploying another contract

---
+++ contracts/ApeFactory.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "./ApeSchool.sol"; 
+
+// A contract that deploys another contract
+contract ApeFactory {
+    ApeSchool[] public apeSchoolArray;
+
+    function createApeSchoolContract() public {
+        ApeSchool apeSchool = new ApeSchool();
+        apeSchoolArray.push(apeSchool);
+    }
+
+    function apeSchoolAddApe(uint256 _index, string memory _name, uint256 _favouriteNumber) public {
+        // Address
+        // ABI (Application Binary Interface)
+        ApeSchool apeSchool = ApeSchool(address(apeSchoolArray[_index]));
+        apeSchool.addApe(_name, _favouriteNumber);
+    }
+
+    function apeSchoolGetApeFavouriteNumber(uint256 _index, string memory _name) public view returns(uint256) {
+        return ApeSchool(address(apeSchoolArray[_index])).getApeFavouriteNumber(_name);
+    }
+}
